e6bac7e5f4bee23d96356aa99c0955209f6716c1
iamai74 <named.me.ai@gmail.com>
2019-10-12 21:19:53 +0500

    Add screen to work with task

--- ArraySort/ViewController.swift
+++ ArraySort/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var arraySizeTextInput: UITextField!
+    @IBOutlet weak var sourceArrayLabel: UILabel!
+    @IBOutlet weak var sortArrayLabel: UILabel!
+    
+    private var sourceArray: [Int] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onDonePressed(_ sender: Any) {
+        let isValidSize = validateArraySize(value: arraySizeTextInput.text)
+        guard isValidSize else {return}
+        let arraySize = Int(arraySizeTextInput.text!)
+        sourceArray = [Int](1...arraySize!)
+        sourceArrayLabel.text = "Source array: \(sourceArray)"
+        var resultArray: [Int] = []
+        resultArray = sourceArray.filter({$0%3 == 0})
+        let otherPartArray = sourceArray.filter({$0%3 != 0}).sorted().reversed()
+        resultArray += otherPartArray
+        sortArrayLabel.text = "Sorted array: \(resultArray)"
+    }
+    
+    private func validateArraySize(value: String?) -> Bool {
+        if value == nil || value!.isEmpty {
+            showErrorAlert(message: "Need to input array size")
+            return false
+        }
+        guard let intValue = Int(value!) else {
+            showErrorAlert(message: "Need to input Integer value")
+            return false
+        }
+        guard intValue > 1 && intValue <= 10000 else {
+            showErrorAlert(message: "Value need to be between 2 and 10000")
+            return false
+        }
+        return true
+    }
+    
+    private func showErrorAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated:true, completion: nil)
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,4 @@
+#  Array Sorting
+## Test App for NapoleonIT School
+Swift5 (xCode 11.1)
+
