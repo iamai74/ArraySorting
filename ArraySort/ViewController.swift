65d0416de892c7e34b383b2e7b7f7dba760178dc
iamai74 <named.me.ai@gmail.com>
2019-10-12 19:20:13 +0500

    Initial Commit

---
+++ ArraySort/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArraySort
+//
+//  Created by AI on 12.10.2019.
+//  Copyright © 2019 iamai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
